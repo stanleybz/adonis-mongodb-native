16c59c4867fcc1fbc1d1f51baab9490d417863d0
Stanley Cheung <hi@stanley.bz>
2018-01-26 14:25:22 +0800

    Update connect to promise

--- src/MongodbNative/index.js
+++ src/MongodbNative/index.js
@@ -9,22 +9,26 @@ class MongodbNative {
     this.database = Config._config.database.mongodb.connection.database;
 
     this.status = 'loading';
-
+    this.db = { collection: null }
+    
     this.connect = this.connect.bind(this);
     this.close = this.close.bind(this);
   }
 
   connect() {
-    MongoClient.connect(this.connStr, function(err, client) {
-      if (err) {
-        console.log(err);
-        this.status = 'error';
-      } else {
-        this.db = client.db(this.database);
-        this.client = client;
-        this.status = 'connected';
-      }
-    }.bind(this));
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(this.connStr, function(err, client) {
+        if (err) {
+          reject(err)
+          this.status = 'error';
+        } else {
+          this.db = client.db(this.database);
+          this.client = client;
+          this.status = 'connected';
+          resolve(client);
+        }
+      }.bind(this));
+    });
   }
 
   close() {
